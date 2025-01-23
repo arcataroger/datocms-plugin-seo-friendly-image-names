0d087823c9cd780a0ae05266846014e62bd54efc
Roger Tuan <r.tuan@datocms.com>
2025-01-22 16:26:58 -0800

    Individual updates now working

--- src/components/SEOFriendlyImageNames.tsx
+++ src/components/SEOFriendlyImageNames.tsx
@@ -7,11 +7,11 @@ import {buildClient, LogLevel} from '@datocms/cma-client-browser';
 // Borrowing a typedef from the plugin SDK. This is what our asset gallery returns in formValues
 type AssetGalleryMetadata = NonNullable<NewUpload['default_field_metadata']>[string] & { upload_id: string };
 type CollectionRecord = Item & { product_type?: string }
-type ImageNeedingUpdate = { id: string, currentFilename: string, slugifiedFilename: string }
+type ImageNeedingUpdate = { id: string, currentBasename: string, slugifiedBasename: string }
 
 const slugifyProductType = (productType: string): string => {
 
-    // TODO replace this with a locale-aware slugify lib, but only if they want localized filenames
+    // TODO replace this with a locale-aware slugify lib, but only if they want localized basenames
 
     const slugified: string = productType.toLocaleLowerCase("en-US")
         .replace(/\s+/g, '-')
@@ -21,24 +21,17 @@ const slugifyProductType = (productType: string): string => {
     return pluralized;
 }
 
-const slugifyImageName = (productInfo: {
+const slugifyImageName = ({productType, productHandle, imgMimeType, numberSuffix}: {
     productType: string,
     productHandle: string
-    imgExtension: string,
     imgMimeType?: string,
     numberSuffix: string | number,
 }): string => {
-    const {productType, productHandle, imgExtension, imgMimeType, numberSuffix} = productInfo;
-
     const mediaType: string = imgMimeType?.startsWith('video') ? 'video' : 'image';
-    return `${slugifyProductType(productType)}-${productHandle}-${mediaType}-${numberSuffix}.${imgExtension}`;
+    return `${slugifyProductType(productType)}-${productHandle}-${mediaType}-${numberSuffix}`;
 }
 
 export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) => {
-
-    // Limited-permission API token from https://vrai.admin.datocms.com/environments/datocms-support-plugin-testing/project_settings/access_tokens/321640/edit
-    // You could also use ctx.currentUserAccessToken instead, but that has all the permissions of the current editor. This way is safer.
-    const accessToken = import.meta.env.VITE_DATOCMS_LIMITED_ACCESS_TOKEN
 
     /** Basic setup **/
         // These are provided to us by the plugin SDK. They are passed from main.tsx.
@@ -68,6 +61,17 @@ export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) =
         }
     }, [collection])
 
+    // Limited-permission API token from https://vrai.admin.datocms.com/environments/datocms-support-plugin-testing/project_settings/access_tokens/321640/edit
+    // You could also use ctx.currentUserAccessToken instead, but that has all the permissions of the current editor. This way is safer.
+    const accessToken = import.meta.env.VITE_DATOCMS_LIMITED_ACCESS_TOKEN
+
+    // Initialize Dato CMA client
+    const client = buildClient({
+        apiToken: accessToken,
+        environment: 'datocms-support-plugin-testing', // Replace with primary once tested
+        logLevel: LogLevel.BASIC, // Or LogLevel.BODY for more debug info
+    })
+
     /** Fetch needed data from the CMA and update our local state **/
 
     useEffect(() => {
@@ -76,13 +80,6 @@ export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) =
             console.log('starting fetches')
 
             try {
-                // Initialize Dato CMA client
-                const client = buildClient({
-                    apiToken: accessToken,
-                    environment: 'datocms-support-plugin-testing', // Replace with primary once tested
-                    logLevel: LogLevel.BODY,
-                })
-
                 // Get the product type from the collection Id
                 const collection = await client.items.find(collectionId)
                 if (collection) {
@@ -117,13 +114,12 @@ export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) =
         })()
     }, [collectionId, galleryItems])
 
-    /** Identify images needing a filename update **/
+    /** Identify images needing a basename update (basename is just the filename without extension, because that's what the CMA wants) **/
 
     const slugifiedImageNames = useMemo<string[]>(() =>
         images.map(img => slugifyImageName({
             productType,
             productHandle,
-            imgExtension: img.format ?? img.filename.match(/\.([0-9a-z]+)(?:[?#]|$)/i)?.[1] ?? '',
             imgMimeType: img.mime_type ?? undefined,
             // numberSuffix: index + 1 // This makes it pretty confusing when they are reordered in the gallery
             numberSuffix: img.md5.slice(0, 5) // We can use a shortened hash instead to better keep track of them
@@ -131,16 +127,32 @@ export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) =
 
     const imagesNeedingUpdate = useMemo<ImageNeedingUpdate[]>(() =>
         images.flatMap(((img, index) => {
-            if (img.filename !== slugifiedImageNames[index]) {
+            if (img.basename !== slugifiedImageNames[index]) {
                 return [{
                     id: img.id,
-                    currentFilename: img.filename,
-                    slugifiedFilename: slugifiedImageNames[index]
+                    currentBasename: img.basename,
+                    slugifiedBasename: slugifiedImageNames[index]
                 }]
             } else {
                 return []
             }
         })), [images, slugifiedImageNames])
+
+    const updateBasename = async ({id, newBasename}: { id: string, newBasename: string }) => {
+        try {
+            const updatedImage = await client.uploads.update(id, {
+                basename: newBasename,
+            })
+
+            if (updatedImage.basename === newBasename) {
+               setImages(prevImages => prevImages.map(prevImg => prevImg.id === updatedImage.id ? updatedImage : prevImg))
+            } else {
+                throw new Error(`Image ${id} basename mismatch even after update: ${updatedImage.basename} does not match ${newBasename}`)
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
 
     return (
         <Canvas ctx={ctx}>
@@ -148,14 +160,18 @@ export const SEOFriendlyImageNames = ({ctx}: { ctx: RenderFieldExtensionCtx }) =
                 Add lorem ipsum
             </Button>
 
-            <h3>Debug:</h3>
-            <h4>Filenames needing update</h4>
+            <h2>Debug:</h2>
+            <h3>{imagesNeedingUpdate.length}/{galleryItems.length} images need a basename update:</h3>
             <ul>
-                {imagesNeedingUpdate.map(img => <li key={img.id}>(#{img.id}) {img.currentFilename} should be {img.slugifiedFilename}</li>)}
+                {imagesNeedingUpdate.map(img => <li key={img.id}>(#{img.id}) {img.currentBasename} should
+                    be {img.slugifiedBasename}. <a href={"#"} onClick={() => updateBasename({
+                        id: img.id,
+                        newBasename: img.slugifiedBasename
+                    })}>Update?</a></li>)}
             </ul>
 
             <ul>
-                <li>{galleryItems.length} images detected</li>
+                <li>{images.length} images cached (out of {galleryItems.length} in gallery)</li>
                 <li>Collection ID: {collectionId}</li>
                 <li>Shopify handle: {productHandle}</li>
                 <li>Product type: {productType}</li>
